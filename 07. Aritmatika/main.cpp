70fb2d2a2f594ae26a0e517ef2ce189c8753a868
kelvinleonardos <kelvinlsianipar@gmail.com>
2023-06-25 21:17:47 +0800

    07. Aritmatika

---
+++ 07. Aritmatika/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+
+    int a = 7;
+    int b = 4;
+
+    int hasil;
+
+    // penjumlahan
+    hasil = a + b;
+    std::cout << hasil << std::endl;
+
+    // pengurangan
+    hasil = a - b;
+    std::cout << hasil << std::endl;
+
+    // perkalian
+    hasil = a * b;
+    std::cout << hasil << std::endl;
+
+    // pembagian
+    hasil = a / b;
+    std::cout << hasil << std::endl;
+
+    // modulus
+    hasil = a % b;
+    std::cout << hasil << std::endl;
+
+    return 0;
+}
---
+++ 07. Aritmatika/readme.md
@@ -0,0 +1,14 @@
+# Aritmatika
+
+## 1. Tambah (+)
+
+## 2. Kurang (-)
+
+## 3. Kali (*)
+
+## 4. Bagi (/)
+> Apabila ingin mendapat hasil desimal, maka salah satu dari bilangan yang dioperasikan harus bertipe desimal (float / double).
+
+## 5. Modulus (%)
+
+> Urutan operasi sesuai operasi matematika.
