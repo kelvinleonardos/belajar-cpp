bee71595a2013427db4ccabfb1ade40f05ce58ae
kelvinleonardos <kelvinlsianipar@gmail.com>
2023-06-25 21:35:37 +0800

    08. Komparasi

---
+++ 08. Komparasi/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+
+    int a = 2;
+    int b = 2;
+
+    // sama dengan
+    std::cout << (a==b) << std::endl;
+
+    // tidak sama dengan
+    std::cout << (a!=b) << std::endl;
+
+    // lebih dari
+    std::cout << (a>b) << std::endl;
+
+    // kurang dari
+    std::cout << (a<b) << std::endl;
+
+    // lebih dari atau sama dengan
+    std::cout << (a>=b) << std::endl;
+
+    // kurang dari atau sama dengan
+    std::cout << (a<=b) << std::endl;
+
+    return 0;
+}
---
+++ 08. Komparasi/readme.md
@@ -0,0 +1,13 @@
+# Komparasi
+
+## 1. Sebanding (==)
+
+## 2. Tidak sebanding (!=)
+
+## 3. Kurand dari (<)
+
+## 4. Lebih dari (>)
+
+## 5. Kurand dari atau sama dengan (<=)
+
+## 6. Lebih dari atau sama dengan (>=)
